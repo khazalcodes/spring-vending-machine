02491e5ead45df10d3d852b9bfa4ca4bdc9fc791
khazalcodes <ammar.m.khazal@gmail.com>
2021-11-13 22:43:41 +0000

    Corrected mistakes in sql init script

--- src/main/resources/db-init-script.sql
+++ src/main/resources/db-init-script.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS vending-machine;
-CREATE DATABASE vending-machine;
+DROP DATABASE IF EXISTS vending_machine;
+CREATE DATABASE vending_machine;
 
-USE vending-machine;
+USE vending_machine;
 
 CREATE TABLE items(
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -10,13 +10,13 @@ CREATE TABLE items(
     amount_in_stock INT DEFAULT 0
 );
 
-INSERT INTO items(name, pric, amount_in_stock)
-VALUES (water, 1.00, 8),
-(coke, 1.50, 10),
-(pepsi, 1.50, 10),
-(mars, 0.90, 10),
-(snickers, 0.90, 10),
-(walkers, 0.75, 10),
-(mccoys, 0.75, 10),
-(apple  juice,1. 20,10),
-(orange  juice,1. 20,10);
+INSERT INTO items(name, price, amount_in_stock)
+VALUES ('water', 1.00, 10),
+('coke', 1.50, 10),
+('pepsi', 1.50, 10),
+('mars', 0.90, 10),
+('snickers', 0.90, 10),
+('walkers', 0.75, 10),
+('mccoys', 0.75, 10),
+('apple juice', 1.20, 10),
+('orange juice', 1.20, 10);
