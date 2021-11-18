8953cf707f1208afcd97bdf6ba66072e2ef8179b
khazalcodes <ammar.m.khazal@gmail.com>
2021-11-18 13:13:13 +0000

    Removed Csv data transferral code in ItemDto since we are now working with a db

--- src/main/java/com/khazalcodes/ItemDto.java
+++ src/main/java/com/khazalcodes/ItemDto.java
@@ -7,35 +7,13 @@ import java.math.RoundingMode;
 
 public class ItemDto implements DbDto {
 
-    private static int CURRENTLY_AVAILABLE_KEY = 0;
     private static final int INVALID_NUMBER = -1;
     private static final int NO_STOCK = 0;
 
     private final int key;
-    private String name;
-    private BigDecimal price;
+    private final String name;
+    private final BigDecimal price;
     private int stockRemaining;
-
-    public ItemDto(String name, String price, String stockRemaining) {
-        this.name = name;
-
-        try {
-            this.price = new BigDecimal(price).setScale(2, RoundingMode.HALF_UP);
-        } catch (NumberFormatException e) {
-            System.out.printf("Invalid price in db for %s... Setting price to -1", name);
-            this.price = BigDecimal.valueOf(INVALID_NUMBER);
-        }
-
-        try {
-            this.stockRemaining = Integer.parseInt(stockRemaining);
-        } catch (NumberFormatException e) {
-            System.out.printf("Invalid stock quantity in db for %s... Setting stock remaining to -1", name);
-            this.stockRemaining = INVALID_NUMBER;
-        }
-
-        this.key = CURRENTLY_AVAILABLE_KEY;
-        CURRENTLY_AVAILABLE_KEY++;
-    }
 
     public ItemDto(int id, String name, double price, int stockRemaining) {
         this.key = id;
@@ -50,22 +28,11 @@ public class ItemDto implements DbDto {
     @Override
     public int getKey() { return key; }
 
-    public String getName() {
-        return name;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
+    public BigDecimal getPrice() { return price; }
 
     public int getStockRemaining() { return stockRemaining; }
 
     public void decrementStock() { this.stockRemaining--; }
-
-    public String toCsvString() {
-        String sep = ",";
-        return name + sep + price.toString() + sep + stockRemaining + System.lineSeparator();
-    }
 
     public boolean isDisplayable() {
         return (!this.price.equals(BigDecimal.valueOf(INVALID_NUMBER))  &&
